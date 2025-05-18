5131211717dff2970b914f47875f1b26897eaed5
Mahanthi Raju <mahanthi0505@gmail.com>
2025-05-18 15:16:41 -0400

    recent changes

--- Food/indian/urls.py
+++ Food/indian/urls.py
@@ -1,5 +1,7 @@
 from indian.views import andhra
 from indian.views import tamil
+from indian.views import login_view
+from indian.views import signup
 from indian.views import northindian
 from indian.views import Indiancuisine
 from django.urls import path
@@ -8,5 +10,7 @@ urlpatterns = [
     path('andhra/',andhra, name ='andhra'),
     path('tamil/',tamil, name ='tamil'),
     path('north/',northindian, name ='north-indian'),
-    path('cuisine/',Indiancuisine, name ='Indian-cuisine')
+    path('cuisine/',Indiancuisine, name ='Indian-cuisine'),
+    path('login/', login_view, name = 'Login'),
+    path('signup/',signup, name = 'Signup')
 ]
--- Food/indian/views.py
+++ Food/indian/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
 
 def andhra(request):
     return HttpResponse("The list of Andhra Dishes")
@@ -12,4 +15,27 @@ def Indiancuisine(request):
     template = loader.get_template('indian.html')
     return HttpResponse(template.render())
 
+def login_view(request):
+    if request.method == 'POST':
+     username = request.POST.get('username')
+     password = request.POST.get('password')
+     user = authenticate(request, username = username,password = password)
+     if user:
+         login(request, user)
+         return redirect("indian")
+     else:
+        messages.error(request, "Invalid username and password, please sign up")
+        return redirect("signup")
+    return render(request, 'login.html')
 
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if user.object.filter(username = username, password = password):
+            messages.erroe(request, "username already exixts.")
+            return redirect('login')
+        user.object.create_user(username = username, password = password)
+        messages.success(request, "sign up successful, Now you can login")
+        return redirect('login')
+    return render(request,'sign.html')
